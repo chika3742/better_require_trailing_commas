662a86bfe00aec08eafe1f32d32b5b75ce9a30f6
chika3742 <kazu.chika.shima@gmail.com>
2026-01-01 01:37:18 +0900

    Rewrite using analysis_server_plugin

---
+++ lib/main.dart
@@ -0,0 +1,296 @@
+import 'dart:async';
+
+import 'package:analysis_server_plugin/edit/dart/correction_producer.dart';
+import 'package:analysis_server_plugin/edit/dart/dart_fix_kind_priority.dart';
+import 'package:analysis_server_plugin/plugin.dart';
+import 'package:analysis_server_plugin/registry.dart';
+import 'package:analyzer/analysis_rule/analysis_rule.dart';
+import 'package:analyzer/analysis_rule/rule_context.dart';
+import 'package:analyzer/analysis_rule/rule_visitor_registry.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/error/error.dart';
+import 'package:analyzer/source/line_info.dart';
+import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
+import 'package:analyzer_plugin/utilities/fixes/fixes.dart';
+import 'package:meta/meta.dart';
+
+final plugin = _BetterRequireTrailingCommasPlugin();
+
+class _BetterRequireTrailingCommasPlugin extends Plugin {
+  @override
+  String get name => "Better Require Trailing Commas";
+
+  @override
+  FutureOr<void> register(PluginRegistry registry) {
+    registry
+      ..registerLintRule(BetterRequireTrailingCommasRule())
+      ..registerFixForRule(BetterRequireTrailingCommasRule.code, _AddTrailingComma.new);
+  }
+}
+
+@visibleForTesting
+class BetterRequireTrailingCommasRule extends AnalysisRule {
+  static const code = LintCode(
+    "better_require_trailing_commas",
+    "Missing trailing comma",
+    correctionMessage: "Try adding a trailing comma.",
+  );
+
+  BetterRequireTrailingCommasRule() : super(
+    name: "better_require_trailing_commas",
+    description: "Enforces trailing commas.",
+  );
+
+  @override
+  DiagnosticCode get diagnosticCode => code;
+
+  @override
+  void registerNodeProcessors(RuleVisitorRegistry registry, RuleContext context) {
+    final visitor = _RequireTrailingCommasVisitor(this, context);
+
+    registry
+      ..addCompilationUnit(this, visitor)
+      ..addArgumentList(this, visitor)
+      ..addAssertInitializer(this, visitor)
+      ..addAssertStatement(this, visitor)
+      ..addFormalParameterList(this, visitor)
+      ..addListLiteral(this, visitor)
+      ..addSetOrMapLiteral(this, visitor)
+      ..addRecordLiteral(this, visitor)
+      ..addRecordPattern(this, visitor)
+      ..addRecordTypeAnnotation(this, visitor)
+      ..addSwitchExpression(this, visitor)
+      ..addEnumDeclaration(this, visitor);
+  }
+}
+
+class _RequireTrailingCommasVisitor extends SimpleAstVisitor<void> {
+  final AnalysisRule rule;
+  final RuleContext context;
+
+  _RequireTrailingCommasVisitor(this.rule, this.context);
+
+  late LineInfo _lineInfo;
+
+  @override
+  void visitCompilationUnit(CompilationUnit node) => _lineInfo = node.lineInfo;
+
+  @override
+  void visitArgumentList(ArgumentList node) {
+    if (node.arguments.isEmpty) return;
+    _checkTrailingComma(
+      openingToken: node.leftParenthesis,
+      closingToken: node.rightParenthesis,
+      firstNode: node.arguments.first,
+      lastNode: node.arguments.last,
+    );
+  }
+
+  @override
+  void visitAssertInitializer(AssertInitializer node) {
+    _checkTrailingComma(
+      openingToken: node.leftParenthesis,
+      closingToken: node.rightParenthesis,
+      firstNode: node.condition,
+      lastNode: node.message ?? node.condition,
+    );
+  }
+  
+  @override
+  void visitAssertStatement(AssertStatement node) {
+    _checkTrailingComma(
+      openingToken: node.leftParenthesis,
+      closingToken: node.rightParenthesis,
+      firstNode: node.condition,
+      lastNode: node.message ?? node.condition,
+    );
+  }
+  
+  @override
+  void visitFormalParameterList(FormalParameterList node) {
+    if (node.parameters.isEmpty) return;
+    _checkTrailingComma(
+      openingToken: node.leftParenthesis,
+      closingToken: node.rightParenthesis,
+      firstNode: node.parameters.first,
+      lastNode: node.parameters.last,
+      errorToken: node.rightDelimiter ?? node.rightParenthesis,
+    );
+  }
+  
+  @override
+  void visitListLiteral(ListLiteral node) {
+    if (node.elements.isEmpty) return;
+    _checkTrailingComma(
+      openingToken: node.leftBracket,
+      closingToken: node.rightBracket,
+      firstNode: node.elements.first,
+      lastNode: node.elements.last,
+    );
+  }
+  
+  @override
+  void visitSetOrMapLiteral(SetOrMapLiteral node) {
+    if (node.elements.isEmpty) return;
+    _checkTrailingComma(
+      openingToken: node.leftBracket,
+      closingToken: node.rightBracket,
+      firstNode: node.elements.first,
+      lastNode: node.elements.last,
+    );
+  }
+  
+  @override
+  void visitRecordLiteral(RecordLiteral node) {
+    if (node.fields.isEmpty) return;
+    _checkTrailingComma(
+      openingToken: node.leftParenthesis,
+      closingToken: node.rightParenthesis,
+      firstNode: node.fields.first,
+      lastNode: node.fields.last,
+    );
+  }
+  
+  @override
+  void visitRecordPattern(RecordPattern node) {
+    if (node.fields.isEmpty) return;
+    _checkTrailingComma(
+      openingToken: node.leftParenthesis,
+      closingToken: node.rightParenthesis,
+      firstNode: node.fields.first,
+      lastNode: node.fields.last,
+    );
+  }
+  
+  @override
+  void visitRecordTypeAnnotation(RecordTypeAnnotation node) {
+    if (node.namedFields == null && node.positionalFields.isEmpty) return;
+    final fields = (node.namedFields?.fields ?? node.positionalFields);
+    _checkTrailingComma(
+      openingToken: node.leftParenthesis,
+      closingToken: node.rightParenthesis,
+      firstNode: fields.first,
+      lastNode: fields.last,
+      errorToken: node.namedFields?.rightBracket ?? node.rightParenthesis,
+    );
+  }
+  
+  @override
+  void visitSwitchExpression(SwitchExpression node) {
+    if (node.cases.isEmpty) return;
+    _checkTrailingComma(
+      openingToken: node.leftBracket,
+      closingToken: node.rightBracket,
+      firstNode: node.cases.first,
+      lastNode: node.cases.last,
+    );
+  }
+  
+  @override
+  void visitEnumDeclaration(EnumDeclaration node) {
+    if (node.constants.isEmpty) return;
+    _checkTrailingComma(
+      openingToken: node.leftBracket,
+      closingToken: node.semicolon ?? node.rightBracket,
+      firstNode: node.constants.first,
+      lastNode: node.constants.last,
+    );
+  }
+
+  void _checkTrailingComma({
+    required Token openingToken,
+    required Token closingToken,
+    required AstNode firstNode,
+    required AstNode lastNode,
+    Token? errorToken,
+  }) {
+    errorToken ??= closingToken;
+
+    // Early exit if trailing comma is present.
+    if (lastNode.endToken.next?.type == TokenType.COMMA) return;
+
+    // If the expression is not intended to be multi-line, ignore it.
+    if (!_isMultiline(openingToken, closingToken, firstNode, lastNode)) {
+      return;
+    }
+
+    rule.reportAtToken(errorToken);
+  }
+
+  /// Returns `true` if the opening and closing tokens are on different lines
+  /// than the child nodes.
+  bool _isMultiline(
+      Token openingToken,
+      Token closingToken,
+      AstNode firstNode,
+      AstNode lastNode,
+      ) {
+    if (!_isSameLine(openingToken, firstNode.beginToken) &&
+        !_isSameLine(closingToken, lastNode.endToken)) {
+      return true;
+    }
+    return false;
+  }
+
+  bool _isSameLine(Token token1, Token token2) =>
+      _lineInfo.getLocation(token1.offset).lineNumber ==
+          _lineInfo.getLocation(token2.end).lineNumber;
+}
+
+class _AddTrailingComma extends ResolvedCorrectionProducer {
+  static const _kind = FixKind(
+    "brtc.fix.addTrailingComma",
+    DartFixKindPriority.standard,
+    "Add a trailing comma",
+  );
+
+  _AddTrailingComma({required super.context});
+
+  @override
+  CorrectionApplicability get applicability => CorrectionApplicability.singleLocation;
+
+  @override
+  FixKind get fixKind => _kind;
+
+  @override
+  Future<void> compute(ChangeBuilder builder) async {
+    Future<void> insertCommaAfter(Token token) {
+      return builder.addDartFileEdit(file, (builder) {
+        builder.addSimpleInsertion(token.end, ",");
+      });
+    }
+
+    switch (node) {
+      case ArgumentList(:final rightParenthesis):
+        await insertCommaAfter(rightParenthesis.previous!);
+      case AssertInitializer(:final rightParenthesis):
+        await insertCommaAfter(rightParenthesis.previous!);
+      case AssertStatement(:final rightParenthesis):
+        await insertCommaAfter(rightParenthesis.previous!);
+      case FormalParameterList(:final rightDelimiter, :final rightParenthesis):
+        await insertCommaAfter(
+          (rightDelimiter ?? rightParenthesis).previous!,
+        );
+      case ListLiteral(:final rightBracket):
+        await insertCommaAfter(rightBracket.previous!);
+      case SetOrMapLiteral(:final rightBracket):
+        await insertCommaAfter(rightBracket.previous!);
+      case RecordLiteral(:final rightParenthesis):
+        await insertCommaAfter(rightParenthesis.previous!);
+      case RecordPattern(:final rightParenthesis):
+        await insertCommaAfter(rightParenthesis.previous!);
+      case RecordTypeAnnotation(:final namedFields, :final rightParenthesis):
+        await insertCommaAfter(
+          (namedFields?.rightBracket ?? rightParenthesis).previous!,
+        );
+      case SwitchExpression(:final rightBracket):
+        await insertCommaAfter(rightBracket.previous!);
+      case EnumDeclaration(:final semicolon, :final rightBracket):
+        await insertCommaAfter(
+          (semicolon ?? rightBracket).previous!,
+        );
+    }
+  }
+}
---
+++ test/better_require_trailing_commas_test.dart
@@ -0,0 +1,414 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:analyzer_testing/analysis_rule/analysis_rule.dart';
+import 'package:better_require_trailing_commas/main.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+void main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(BetterRequireTrailingCommasTest);
+  });
+}
+
+@reflectiveTest
+class BetterRequireTrailingCommasTest extends AnalysisRuleTest {
+  @override
+  void setUp() {
+    rule = BetterRequireTrailingCommasRule();
+    super.setUp();
+  }
+
+  void test_argumentListWithoutComma() async {
+    await assertDiagnostics(r"""
+void fn({String? arg1}) {}
+void f() {
+  fn(
+    arg1: "value"
+  );
+}
+    """, [lint(64, 1)]);
+  }
+
+  void test_argumentListWithComma() async {
+    await assertNoDiagnostics(r"""
+void fn({String? arg1}) {}
+void f() {
+  fn(
+    arg1: "value",
+  );
+}
+    """);
+  }
+
+
+  void test_assertInitializerWithoutComma() async {
+    await assertDiagnostics(r"""
+class Test {
+  const Test(String foo)
+      : assert(
+          foo == "bar",
+          "foo must be 'bar'"
+        );
+}
+    """, [lint(116, 1)]);
+  }
+
+  void test_assertInitializerWithComma() async {
+    await assertNoDiagnostics(r"""
+class Test {
+  const Test(String foo)
+      : assert(
+          foo == "bar",
+          "foo must be 'bar'",
+        );
+}
+    """);
+  }
+
+
+  void test_assertStatementWithoutComma() async {
+    await assertDiagnostics(r"""
+void fn() {
+  final foo = "bar";
+  assert(
+    foo == "bar",
+    "foo must be 'bar'"
+  );
+}
+    """, [lint(87, 1)]);
+  }
+
+  void test_assertStatementWithComma() async {
+    await assertNoDiagnostics(r"""
+void fn() {
+  final foo = "bar";
+  assert(
+    foo == "bar",
+    "foo must be 'bar'",
+  );
+}
+    """);
+  }
+
+
+  void test_formalParameterListWithoutComma() async {
+    await assertDiagnostics(r"""
+void fn1({
+  String? foo,
+  String? bar
+}) {}
+void fn2([
+  String? foo,
+  String? bar
+]) {}
+void fn3(
+  String? foo,
+  String? bar
+) {}
+void fn4(
+  String? foo,
+  String? bar, {
+  String? baz
+}) {}
+    """, [
+      lint(40, 1),
+      lint(86, 1),
+      lint(131, 1),
+      lint(192, 1),
+    ]);
+  }
+
+  void test_formalParameterListWithComma() async {
+    await assertNoDiagnostics(r"""
+void fn1({
+  String? foo,
+  String? bar,
+}) {}
+void fn2([
+  String? foo,
+  String? bar,
+]) {}
+void fn3(
+  String? foo,
+  String? bar,
+) {}
+void fn4(
+  String? foo,
+  String? bar, {
+  String? baz,
+}) {}
+    """);
+  }
+
+
+  void test_listLiteralWithoutComma() async {
+    await assertDiagnostics(r"""
+final list = [
+  1,
+  2
+];
+    """, [
+      lint(24, 1),
+    ]);
+  }
+
+  void test_listLiteralWithComma() async {
+    await assertNoDiagnostics(r"""
+final list = [
+  1,
+  2,
+];
+    """);
+  }
+
+
+  void test_setOrMapLiteralWithoutComma() async {
+    await assertDiagnostics(r"""
+final set = {
+  1,
+  2
+};
+final map = {
+  1: "one",
+  2: "two"
+};
+    """, [
+      lint(23, 1),
+      lint(63, 1),
+    ]);
+  }
+
+  void test_setOrMapLiteralWithComma() async {
+    await assertNoDiagnostics(r"""
+final set = {
+  1,
+  2,
+};
+final map = {
+  1: "one",
+  2: "two",
+};
+    """);
+  }
+
+
+  void test_recordLiteralWithoutComma() async {
+    await assertDiagnostics(r"""
+final record1 = (
+  1,
+  2
+);
+final record2 = (
+  one: 1,
+  two: 2
+);
+    """, [
+      lint(27, 1),
+      lint(67, 1),
+    ]);
+  }
+
+  void test_recordLiteralWithComma() async {
+    await assertNoDiagnostics(r"""
+final record1 = (
+  1,
+  2,
+);
+final record2 = (
+  one: 1,
+  two: 2,
+);
+    """);
+  }
+
+
+  void test_recordPatternWithoutComma() async {
+    await assertDiagnostics(r"""
+void fn () {
+  final (
+    :name,
+    :age
+  ) = (name: "foo", age: 20);
+
+  final (
+    one,
+    two
+  ) = (1, 2);
+}
+    """, [
+      lint(45, 1),
+      lint(103, 1),
+    ]);
+  }
+
+  void test_recordPatternWithComma() async {
+    await assertNoDiagnostics(r"""
+void fn6 () {
+  final (
+    :name,
+    :age,
+  ) = (name: "foo", age: 20);
+
+  final (
+    one,
+    two,
+  ) = (1, 2);
+}
+    """);
+  }
+
+
+  void test_recordTypeAnnotationWithoutComma() async {
+    await assertDiagnostics(r"""
+typedef recordType1 = (
+  int,
+  int
+);
+typedef recordType2 = ({
+  String name,
+  int age
+});
+    """, [
+      lint(37, 1),
+      lint(90, 1),
+    ]);
+  }
+
+  void test_recordTypeAnnotationWithComma() async {
+    await assertNoDiagnostics(r"""
+typedef recordType1 = (
+  int,
+  int,
+);
+typedef recordType2 = ({
+  String name,
+  int age,
+});
+    """);
+  }
+
+
+  void test_switchExpressionWithoutComma() async {
+    await assertDiagnostics(r"""
+void fn() {
+  final success = true;
+  final foo = switch (success) {
+    true => "success",
+    false => "failure"
+  };
+}
+    """, [
+      lint(117, 1),
+    ]);
+  }
+
+  void test_switchExpressionWithComma() async {
+    await assertNoDiagnostics(r"""
+void fn() {
+  final success = true;
+  final foo = switch (success) {
+    true => "success",
+    false => "failure",
+  };
+}
+    """);
+  }
+
+
+  void test_enumDeclarationWithoutComma() async {
+    await assertDiagnostics(r"""
+enum FooState1 {
+  bar,
+  baz
+}
+enum FooState2 {
+  bar,
+  baz
+  ;
+}
+
+enum BarState {
+  taro("taro"),
+  hanako("hanako")
+  ;
+
+  const BarState(this.name);
+
+  final String name;
+}
+    """, [
+      lint(30, 1),
+      lint(64, 1),
+      lint(122, 1),
+    ]);
+  }
+
+  void test_enumDeclarationWithComma() async {
+    await assertNoDiagnostics(r"""
+enum FooState1 {
+  bar,
+  baz,
+}
+enum FooState2 {
+  bar,
+  baz,
+  ;
+}
+enum FooState3 {
+  bar,
+  baz;
+}
+
+enum BarState {
+  taro("taro"),
+  hanako("hanako"),
+  ;
+
+  const BarState(this.name);
+
+  final String name;
+}
+    """);
+  }
+
+
+  void test_useEffect() async {
+    await assertDiagnostics(r"""
+void fn() {
+  useEffect(() {
+    // do something
+  }, []);
+  useEffect(
+    () {
+      // do something
+    },
+    []
+  );
+}
+void useEffect(void Function() cb, List<Object> dependencies) {}
+    """, [
+      lint(119, 1),
+    ]);
+  }
+
+  void test_multilineException() async {
+    await assertDiagnostics(r"""
+void fn() {
+  Foo(Bar(
+    baz: "baz",
+  ));
+  Foo(
+    Bar(baz: "baz")
+  );
+}
+class Foo {
+  final Object foo;
+  Foo(this.foo);
+}
+class Bar {
+  final String baz;
+  Bar({required this.baz});
+}
+    """, [lint(74, 1)]);
+  }
+}
